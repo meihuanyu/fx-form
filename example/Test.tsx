97c0f90e403b4b4a6be5078124d971be8d529eaa
meihuanyu <763084024@qq.com>
2019-10-31 15:44:18 +0800

    update config

--- example/Test.tsx
+++ example/Test.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import FreeForm, { FormItem } from '../lib'
+import FreeForm, { FormItem } from 'react-free-form'
 import { Input, Button } from 'antd';
 import { IFreeForm } from '../lib/useForm';
 
 export default () => {
     let form: IFreeForm | null = null
     const initValues = {}
-    return <FreeForm initValues = {initValues} init = {f => form = f}>
-                <FormItem name = "entryName" label = "风控主体名称" rules = "required">
-                    <Input />
-                </FormItem>
-                <Button onClick = {() => form && console.log(form.onSubmit())}>提交</Button>
-            </FreeForm>
+    console.log(FreeForm)
+    return <span>xxx</span>
 }
 
